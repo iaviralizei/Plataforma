8b4e717a5985f0a7ba337ceb63a2d5c6c00220ff
iaviralizei <iaviralizei@gmail.com>
2025-06-24 13:35:55 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1151,7 +1151,7 @@ class NewPages:
             <div class="package-badge">POPULAR</div>
             <div class="package-header">
                 <h3 style="color: #9400d3;">PREMIUM</h3>
-                <div class="package-price" style="color: #9400d3;">R$ 99,90</div>
+                <div class="package-price" style="color: #9400d3;">R$ 39,69</div>
                 <small>experiência completa</small>
             </div>
             <ul class="package-benefits">
